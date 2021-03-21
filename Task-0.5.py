8000d0848161ebd5b71443f43565aea68555125f
unknown <sabelombatha15@gmail.com>
2021-03-21 14:56:11 +0200

    Edited Task 0.5

--- Task-0.5.py
+++ Task-0.5.py
@@ -1,20 +1,10 @@
-""" Sabelo Mbatha's Task 0.5 """
+def area_of_triangle(s1, s2, s3):
 
-def area_of_triangle():
+    perimeter = s1 + s2 + s3
+    semi_perimeter = perimeter/2
 
-    s1 = float(input("Enter length of 1st side: "))
-    s2 = float(input("Enter length of 2nd side: "))
-    s3 = float(input("Enter length of 3rd side: "))
+    Area = (((semi_perimeter - s1) * (semi_perimeter - s2) * (semi_perimeter-s3)) * semi_perimeter) * 1/2
 
-    # Calculating the semi-perimeter (s)
-    p = s1 + s2 + s3
-    s = p/2
+    return round(Area, 2)
 
-    # Now for Area
-    z = (s-s1) * (s-s2) * (s-s3)
-    Area = (z * s) * 1/2
-
-    print()
-    print(round(Area, 2))
-
-area_of_triangle()
+area_of_triangle(5, 6, 2)
