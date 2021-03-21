6ea29f3c8cc256701afe35032fb874a14cc03b44
unknown <sabelombatha15@gmail.com>
2021-03-21 15:05:10 +0200

    Edited Task 0.6

--- Task-0.6.py
+++ Task-0.6.py
@@ -1,9 +1,6 @@
-""" Sabelo Mbatha's Task 0.6 """
+def maximum(num1, num2, num3, num4):
 
-
-def maximum(n1, n2, n3, n4):
-
-    num = max(n1, n2, n3, n4)
-    print("The maximum number is {}".format(num))
+    new_num = max(num1, num2, num3, num4)
+    return "The maximum number is {}".format(new_num)
 
 maximum(1,22,3,2)
