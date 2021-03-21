0e72294b86edce7fc883c7f6dfffd08e1fad717b
unknown <sabelombatha15@gmail.com>
2021-03-21 14:58:04 +0200

    Edited Task 0.5

--- Task-0.5.py
+++ Task-0.5.py
@@ -1,9 +1,9 @@
-def area_of_triangle(s1, s2, s3):
+def area_of_triangle(side1, side2, side3):
 
-    perimeter = s1 + s2 + s3
+    perimeter = side1 + side2 + side3
     semi_perimeter = perimeter/2
 
-    Area = (((semi_perimeter - s1) * (semi_perimeter - s2) * (semi_perimeter-s3)) * semi_perimeter) * 1/2
+    Area = (((semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter-side3)) * semi_perimeter) * 1/2
 
     return round(Area, 2)
 
