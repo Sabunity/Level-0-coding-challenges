e2338114a06253be223f5549a3f57582e5e480a2
unknown <sabelombatha15@gmail.com>
2021-03-21 18:02:44 +0200

    Edited Task 0.8

--- Task-0.8.py
+++ Task-0.8.py
@@ -1,20 +1,15 @@
-""" Sabelo Mbatha's Task 0.8 """
+def time_converter(num):
 
-def timeConverter():
+    hours = int(num/60)
+    minutes = num % 60
 
-    # Extracting the time
-    num = int(input("Enter any number: "))
-    a = int(num/60)
-    b = num % 60
-
-    # Printing the correct time label
-    if a>1 and b>1:
-        print("{} hours,".format(a), "{} minutes".format(b))
-    elif a<2 and b>1:
-        print("{} hour,".format(a), "{} minutes".format(b))
-    elif a>1 and b<2:
-        print("{} hours,".format(a), "{} minute".format(b))
+    if hours>1 and minutes>1:
+        print("{} hours,".format(hours), "{} minutes".format(minutes))
+    elif hours<2 and minutes>1:
+        print("{} hour,".format(hours), "{} minutes".format(minutes))
+    elif hours>1 and minutes<2:
+        print("{} hours,".format(hours), "{} minute".format(minutes))
     else:
-        print("{} hour,".format(a), "{} minute".format(b))
+        print("{} hour,".format(hours), "{} minute".format(minutes))
 
-timeConverter()
+time_converter(71)
