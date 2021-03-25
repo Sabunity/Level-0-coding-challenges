eea525e0dc5ac8379db3402ad950d1a0a0a1cd8c
unknown <sabelombatha15@gmail.com>
2021-03-25 04:13:15 +0200

    Edited the Task 0.7

--- Task-0.7.py
+++ Task-0.7.py
@@ -1,28 +1,10 @@
-""" Sabelo Mbatha's Task 0.7 """
+def celsius_to_fahrenheit(celsius_temp):
+    answer = (float(celsius_temp) * 9/5) + 32
+    return round(answer, 2)
 
-def To_Fahrenheit():
-    cel = input("Input temperature: ")
-    answer = (float(cel) * 9/5) + 32
-    print("{} Degrees Fahrenheit".format(round(answer, 2)))
+def fahrenheit_to_celsius(fahrenheit_temp):
+    answer2 = (float(fahrenheit_temp) - 32) * 5/9
+    return round(answer2, 2)
 
-def To_Celsius():
-    fah = input("Input temperature: ")
-    answer2 = (float(fah) - 32) * 5/9
-    print("{} Degrees Celsius".format(round(answer2, 2)))
-
-def options():
-    print(""" Welcome to the temperature degrees conversion zone
-          [1] - Convert from Celsius to Fahrenheit
-          [2] - Convert from Fahrenheit to Celsius
-          """)
-
-    action = input("What would you like to do today? (Enter a number): ")
-
-    if action == "1":
-        To_Fahrenheit()
-    elif action == "2":
-        To_Celsius()
-    else:
-        print("No valid choice was given, try again!")
-
-options()
+celsius_to_fahrenheit(100)
+fahrenheit_to_celsius(212)
