21fb66c3af65e3b0cc35427cafb0a1eb91f6ed85
unknown <sabelombatha15@gmail.com>
2021-03-25 03:42:14 +0200

    Edited the Task 0.4

--- Task-0.4.py
+++ Task-0.4.py
@@ -1,13 +1,10 @@
-""" Sabelo Mbatha's Task 0.4 """
-
-def odd_or_even(num):
-    x = num % 2
-    if x == 0:
-        print()
+def odd_or_even():
+    num = int(input())
+    if num % 2 == 0:
         print("even")
 
     else:
-        print()
         print("odd")
 
-newNum = odd_or_even(int(input("Insert your number: ")))
+
+odd_or_even()
